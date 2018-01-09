5282b82ecad2d910f5eab5d2a78db9b579b3c0c6
Aldo <agordillos@gmail.com>
2018-01-09 13:53:56 +0100

    Config for production

--- app/config/config.js
+++ app/config/config.js
@@ -1,10 +1,23 @@
-export const GLOBAL_CONFIG = {
-  debug:true,
-  debug_scorm_api:true,
-  debug_scorm_api_window:false,
+export let GLOBAL_CONFIG = {
+  dev:{
+    debug:true,
+    debug_scorm_api:true,
+    debug_scorm_api_window:false,
+  },
+  production:{
+    debug:false,
+    debug_scorm_api:false,
+    debug_scorm_api_window:false,
+  },
 };
 
 let processConfig = (function(){
+  let env = process.env.NODE_ENV || 'dev';
+  if(typeof GLOBAL_CONFIG[env] === "undefined"){
+    env = "dev";
+  }
+  GLOBAL_CONFIG = GLOBAL_CONFIG[env];
+
   GLOBAL_CONFIG.debug_scorm_api = ((GLOBAL_CONFIG.debug) && (GLOBAL_CONFIG.debug_scorm_api));
   GLOBAL_CONFIG.debug_scorm_api_window = ((GLOBAL_CONFIG.debug_scorm_api) && (GLOBAL_CONFIG.debug_scorm_api_window));
 })();
